94323ec0cd3ceebda31a27e8e98ce83878c5f585
redouane <izegh.nizar@gmail.com>
2025-06-29 21:38:40 +0200

    Commit Integration Test

---
+++ Api_Store.TestIntegration/DatabaseConnectionTests.cs
@@ -0,0 +1,36 @@
+﻿using Api_Store.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Api_Store.TestIntegration
+    {
+    public class DatabaseConnectionTests
+        {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseConnectionTests()
+            {
+            // Charge appsettings.json depuis le projet principal
+            _configuration = new ConfigurationBuilder()
+     .AddJsonFile("appsettings.Development.json")
+     .Build();
+
+            }
+        [Fact]
+        public async Task CanConnectToDatabase()
+            {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            using var context = new DataContext(optionsBuilder.Options);
+
+            var canConnect = await context.Database.CanConnectAsync();
+
+            Assert.True(canConnect, "Échec de la connexion à la base de données !");
+            }
+
+        }
+    }
